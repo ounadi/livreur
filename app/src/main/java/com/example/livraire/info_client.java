c90f9651a3df195eb055a5746d5fa8eeafbbe769
ounadi <karoumateb@yahoo.fr>
2019-04-03 22:36:32 +0200

    first commit

---
+++ app/src/main/java/com/example/livraire/info_client.java
@@ -0,0 +1,13 @@
+package com.example.livraire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class info_client extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_client);
+    }
+}
